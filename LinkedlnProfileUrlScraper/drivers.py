6109e47db448573d8556c172abb74483f26211ea
Yudhishthirsh <130043970+Yudhishthirsh@users.noreply.github.com>
2023-05-18 16:19:30 +0530

    Update drivers.py

--- LinkedlnProfileUrlScraper/drivers.py
+++ LinkedlnProfileUrlScraper/drivers.py
@@ -5,4 +5,4 @@ chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.default_content_setting_values.notifications": 2}
 chrome_options.add_experimental_option("prefs", prefs)
 from selenium.webdriver.support.wait import WebDriverWait
-driver = webdriver.Chrome(parameter.relative_path, chrome_options=chrome_options)
+driver = webdriver.Chrome(parameter.chromeDriverPath, chrome_options=chrome_options)
